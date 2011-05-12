2dcfbcd32f5cd0b55df121ea1b4f6524f9bc58c6
Christian Biggins <cbiggins@gmail.com>
2011-05-12 17:26:47 +1000

    Adding google analytics

--- index.php
+++ index.php
@@ -119,10 +119,10 @@
   <![endif]-->
 
   <script>
-    // var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID 
-    // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    // g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-    // s.parentNode.insertBefore(g,s)}(document,"script"));
+    var _gaq=[["_setAccount","UA-2740110-12"],["_trackPageview"]]; 
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s)}(document,"script"));
   </script>
 
   <script type="text/javascript">
